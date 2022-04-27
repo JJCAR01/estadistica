01ad33fec45936958435fe7fa09de0e1ad68196c
JUANCARDONA <juanjo-cardona@hotmail.com>
2022-04-27 12:22:34 -0500

    Corrigiendo tests 2

--- src/test/java/com/juan/estadistica/infraestructura/testdatabuilder/DtoJugadorTestDataBuilder.java
+++ src/test/java/com/juan/estadistica/infraestructura/testdatabuilder/DtoJugadorTestDataBuilder.java
@@ -5,17 +5,24 @@ import com.juan.estadistica.aplicacion.dto.DtoJugador;
 public class DtoJugadorTestDataBuilder {
 
     private String nombre;
+    private String apellido;
     private String numeroIdentificacion;
     private byte dorsal;
 
     public DtoJugadorTestDataBuilder() {
         this.nombre = "Juan";
+        this.apellido = "Cardona";
         this.numeroIdentificacion = "1289231929";
         this.dorsal = 4;
     }
 
     public DtoJugadorTestDataBuilder conNombre(String nombre) {
         this.nombre = nombre;
+        return this;
+    }
+
+    public DtoJugadorTestDataBuilder conApellido(String apellido) {
+        this.apellido = apellido;
         return this;
     }
 
@@ -28,5 +35,5 @@ public class DtoJugadorTestDataBuilder {
         return this;
     }
 
-    public DtoJugador build(){return new DtoJugador(nombre,numeroIdentificacion,dorsal);}
+    public DtoJugador build(){return new DtoJugador(nombre,apellido,numeroIdentificacion,dorsal);}
 }
